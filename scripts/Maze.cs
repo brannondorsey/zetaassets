e8e1548061bc6e8e417c496be8d53698d0ae943a
Brannon Dorsey <brannon@brannondorsey.com>
2013-09-08 16:28:52 -0500

    Letter indexing is now working for horizontal walls

--- scripts/Maze.cs
+++ scripts/Maze.cs
@@ -20,20 +20,15 @@ public class Maze : MonoBehaviour {
 	void Start () {
 		CreateFloor();
 		GameObject[] horizontalWalls = new GameObject[numbRows*numbRows];
+		GameObject[] verticalWalls = new GameObject[numbRows*numbRows];
 		int i = 0;
 		char letter = 'A';
 		for(int x = 0; x < horizontalWalls.Length/numbRows; x++){
-			//if x is even 
-			if(x % 2 == 0) letter++;
 				for(int z = 0; z < horizontalWalls.Length/numbRows; z++){
-					horizontalWalls[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-					horizontalWalls[i].AddComponent<Wall>().index = letter+""; //add the Wall class script
-					horizontalWalls[i].name = "Horizontal Wall"; //name it
-					horizontalWalls[i].transform.position = new Vector3(x*wallSize, 0, z*wallSize); //place it
-					horizontalWalls[i].transform.localScale = new Vector3(wallSize, wallSize, wallDepth); //scale it
-					//horizontalWalls[i].GetComponent<Wall>.index = "A1";
+					horizontalWalls[i] = initWall("Horizontal Wall", letter+"", wallSize, wallDepth, x, 0, z);
 					i++;
 				}
+			letter++;
 		}
 	}
 	
@@ -46,6 +41,15 @@ public class Maze : MonoBehaviour {
 		floor = GameObject.CreatePrimitive(PrimitiveType.Plane);
 		floor.transform.localScale = new Vector3(numbRows/2f, 1, (numbRows/2f)-0.5f);
 		floor.transform.position = new Vector3(size/2f-wallSize/2-wallDepth, -wallSize/2f, size/2f-wallSize/2-wallDepth);
+	}
+	
+	protected GameObject initWall(string name, string index, int wallSize, float wallDepth, int x, int y, int z){
+		GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		wall.AddComponent<Wall>().index = index; //add the Wall class script
+		wall.name = "Horizontal Wall"; //name it
+		wall.transform.position = new Vector3(x*wallSize, y, z*wallSize); //place it
+		wall.transform.localScale = new Vector3(wallSize, wallSize, wallDepth); //scale it
+		return wall;
 	}
 
 }
