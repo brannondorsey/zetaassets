b1b439946704813100373524d8fe76be6d6cb70a
Brannon Dorsey <brannon@brannondorsey.com>
2013-09-18 12:49:44 -0500

    Added images

--- scripts/Maze.cs
+++ scripts/Maze.cs
@@ -25,12 +25,20 @@ public class Maze : MonoBehaviour {
 		GameObject[] verticalWalls = new GameObject[numbRows*numbRows];
 		int i = 0;
 		char letter = 'A';
+		int textureFilename = 0;
 		int limit = horizontalWalls.Length/numbRows;
 		for(int x = 0; x < limit; x++){
 				int number = 1;
 				for(int z = 0; z < limit; z++){
-					if(x != limit-1) horizontalWalls[i] = initWall ("Horizontal Wall", letter+number.ToString(), wallSize, wallDepth, new Vector3(x*(wallSize-wallDepth), 0, z*(wallSize-wallDepth)), new Vector3(0, 0, 0));
-					if(z != limit-1) verticalWalls[i] = initWall ("Vertical Wall", letter+number.ToString(), wallSize, wallDepth, new Vector3(x*(wallSize-wallDepth)-(wallSize-wallDepth)/2, 0, z*(wallSize-wallDepth)+(wallSize-wallDepth)/2), new Vector3(0, 90, 0));
+					if(x != limit-1){
+						horizontalWalls[i] = initWall (textureFilename.ToString(),"Horizontal Wall", letter+number.ToString(), wallSize, wallDepth, new Vector3(x*(wallSize-wallDepth), 0, z*(wallSize-wallDepth)), new Vector3(0, 0, 0));
+						textureFilename++;
+					}
+					if(z != limit-1){
+						verticalWalls[i] = initWall (textureFilename.ToString(), "Vertical Wall", letter+number.ToString(), wallSize, wallDepth, new Vector3(x*(wallSize-wallDepth)-(wallSize-wallDepth)/2, 0, z*(wallSize-wallDepth)+(wallSize-wallDepth)/2), new Vector3(0, 90, 0));
+						textureFilename++;
+					}
+					if(textureFilename >= 99) textureFilename = 0; //remove this once there are 200 pictures in the folder
 					number++;
 					i++;
 				}
@@ -46,16 +54,18 @@ public class Maze : MonoBehaviour {
 	protected void CreateFloor(){
 		floor = GameObject.CreatePrimitive(PrimitiveType.Plane);
 		floor.transform.localScale = new Vector3(numbRows/2f, 1, (numbRows/2f));
-		floor.transform.position = new Vector3(size/2f-wallSize/2, -wallSize/2f, size/2f-wallDepth);
+		floor.transform.position = new Vector3(size/2f-wallSize/2-wallDepth/2, -wallSize/2f, size/2f-wallDepth);
 	}
 	
-	protected GameObject initWall(string name, string index, int wallSize, float wallDepth, Vector3 position, Vector3 rotation){
+	protected GameObject initWall(string filename, string name, string index, int wallSize, float wallDepth, Vector3 position, Vector3 rotation){
 		GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		wall.transform.parent = transform;
 		wall.AddComponent<Wall>().index = index; //add the Wall class script
-		wall.name = "Horizontal Wall"; //name it
+		wall.name = name; //name it
 		wall.transform.position = position; //place it
 		wall.transform.Rotate(rotation); //rotate it
 		wall.transform.localScale = new Vector3(wallSize, wallSize, wallDepth); //scale it
+		wall.renderer.material.mainTexture = Resources.Load("test_images/" + filename.ToString()) as Texture;
 		return wall;
 	}
 
